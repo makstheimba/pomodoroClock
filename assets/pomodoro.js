aaf67f6b4c993a47b591cb1a545cdaa5549aa9f5
makstheimba <makstheimba@yandex.ru>
2016-10-10 19:28:59 +0300

    Implemented timer

--- assets/pomodoro.js
+++ assets/pomodoro.js
@@ -1,18 +1,64 @@
+function clickClock(action) {
+    return function () {
+        var count = parseFloat($("#clock").html(), 10) * 1000, // Get value of clock in milliseconds
+            interval = 100;
+            counter = setInterval(timer, interval);
+        $("#clock").off("click").click(pauseClock(counter, action));// Change click event so it pauses the timer
+        disableControls();
+        console.log("inside clickClock", count);
+        function timer() {
+            count -= interval;
+            console.log("inside timer", count);
+            if (count < 0){
+                //clearInterval(counter);//counter ended, do something here
+                action = action === "#session" ? "#break" : "#session";
+                $("#clock").off("click").click(pauseClock(counter, action)); // Change click corresponding to current action
+                count = parseFloat($(action).val(), 10) * 1000;
+            }
+            $("#clock").html(count/1000);
+        }
+    }
+}
+function pauseClock(counter, action) {
+    return function () {
+        clearInterval(counter);
+        console.log("inside pauseClock", counter);
+        enableControls();
+        $("#clock").off("click").click(clickClock(action)); // Change click event so it starts the timer
+    }
+}
+function changeClock() {
+    $("#clock").html($("#session").val());
+    $("#clock").off("click").click(clickClock("#session"));
+}
+
+function changeValue(sign, id){
+    return function () {
+        var currentValue = parseInt($(id).val(), 10);
+        if (sign === "+") 
+            $(id).val(currentValue + 1);
+        else if (currentValue !== 1)
+            $(id).val(currentValue - 1);
+        if (id === "#session")
+            changeClock();
+    }
+}
+function enableControls() {
+    $("#breakMinus").click(changeValue("-", "#break"));
+    $("#breakPlus").click(changeValue("+", "#break"));
+    $("#sessionMinus").click(changeValue("-", "#session"));
+    $("#sessionPlus").click(changeValue("+", "#session"));
+}
+
+function disableControls() {
+    $("#breakMinus").off("click");
+    $("#breakPlus").off("click");
+    $("#sessionMinus").off("click");
+    $("#sessionPlus").off("click");
+}
+
 $(document).ready(function () {
     $("#clock").html($("#session").val());
-    $("#clock").click(function (){
-        var count = parseInt($("#clock").html(), 10) * 1000; //Get milliseconds
-        counter = setInterval(timer, 100);
-        function timer()
-            {
-              count -= 100;
-              if (count < 0)
-              {
-                 clearInterval(counter);
-                 //counter ended, do something here
-                 return;
-              }
-                $("#clock").html(count);
-            }        
-    });
+    $("#clock").click(clickClock("#session"));
+    enableControls();
 });
