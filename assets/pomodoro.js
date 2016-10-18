d26f72fa669343ab38c629dc42a2afc6aa5caf73
makstheimba <makstheimba@yandex.ru>
2016-10-18 17:23:12 +0300

    Styling and buttons

--- assets/pomodoro.js
+++ assets/pomodoro.js
@@ -1,65 +1,108 @@
-function clickClock(action) {
-    return function () {
-        var count = parseFloat($("#clock").html(), 10) * 1000, // Get value of clock in milliseconds
-            interval = 100;
-            counter = setInterval(timer, interval);
-        $("#clock").off("click").click(pauseClock(counter, action));// Change click event so it pauses the timer
-        disableControls();
-        animateProgress("#clock");
-        console.log("inside clickClock", count);
-        function timer() {
-            count -= interval;
-            console.log("inside timer", count);
-            if (count < 0){
-                //clearInterval(counter);//counter ended, do something here
-                action = action === "#session" ? "#break" : "#session";
-                $("#clock").off("click").click(pauseClock(counter, action)); // Change click corresponding to current action
-                count = parseFloat($(action).val(), 10) * 1000;
+var clockMaker = function (clockId) {
+   var minute = 60000,
+       session = 25*minute,
+       rest = 5*minute,
+       timer = session,
+       interval = 1000,
+       running = "session",
+       counter = false;
+    function convertMs(ms){
+        return (("0" + (ms / minute << 0)).slice(-2) + ":" + ("0" + ms % minute / 1000).slice(-2));
+    }
+    
+    return {
+        start: function () {
+            console.log("start", this);
+            var self = this;
+            if (counter === false) // Start the counter if there is none
+                counter = setInterval(function(){countdown();}, interval);
+            else // Pause the timer if counter is running
+                this.pause();            
+            function countdown() {
+                console.log(counter);
+                timer = timer - interval;
+                console.log(timer);
+                if (timer < 0) {
+                    if (running === "session"){
+                        timer = rest;
+                        running = "rest";
+                    } else {
+                        timer = session;
+                        running = "session";
+                    }
+                }
+                self.setTimerToId();
             }
-            $("#clock").html(count/1000);
+            
+        },
+        pause: function () {
+            console.log("pause");
+            clearInterval(counter);
+            counter = false;
+            console.log(counter);
+        },
+        resetClock: function () {
+            console.log("reset");
+            this.pause();
+            running = "session";
+            timer = session;
+            this.setTimerToId();
+        },
+        increaseRest: function () {
+            console.log("increase rest");
+            if (rest < minute * 59){
+                rest += minute;
+            }
+            this.resetClock();
+        },
+        decreaseRest: function () {
+            if (rest > minute) {
+                rest -= minute;
+            }            
+            this.resetClock();
+        },
+        increaseSession: function () {
+            if (session < minute * 59) {
+                session += minute;                
+            }
+            this.resetClock();
+        },
+        decreaseSession: function () {
+            if (session > minute) {
+                session -= minute;
+            }
+            this.resetClock();
+        },
+        getSessionLength: function () {
+            return convertMs(session);
+        },
+        getRestLength: function () {
+            return convertMs(rest);
+        },
+        setTimerToId: function () {
+            $(clockId).html(convertMs(timer));
         }
     }
-}
+};
 
-function pauseClock(counter, action) {
-    return function () {
-        clearInterval(counter);
-        console.log("inside pauseClock", counter);
-        enableControls();
-        $("#clock").off("click").click(clickClock(action)); // Change click event so it starts the timer
-    }
+window.onload = function () {
+    
+    clock = clockMaker("#clock");
+    $("#sessionPlus").on("click", function () {
+        clock.increaseSession();
+        $("#session").val(clock.getSessionLength());
+    });
+    $("#sessionMinus").on("click", function () {
+        clock.decreaseSession();
+        $("#session").val(clock.getSessionLength());
+    });
+    $("#restPlus").on("click", function () {
+        clock.increaseRest();
+        $("#rest").val(clock.getRestLength());
+    });
+    $("#restMinus").on("click", function () {
+        clock.decreaseRest();
+        $("#rest").val(clock.getRestLength());
+    });
+    $(".clockWrap").on("click", function () {clock.start();});
 }
-function changeClock() {
-    $("#clock").html($("#session").val());
-    $("#clock").off("click").click(clickClock("#session"));
-}
-
-function changeValue(sign, id){
-    return function () {
-        var currentValue = parseInt($(id).val(), 10);
-        if (sign === "+") 
-            $(id).val(currentValue + 1);
-        else if (currentValue !== 1)
-            $(id).val(currentValue - 1);
-        changeClock();
-    }
-}
-function enableControls() {
-    $("#breakMinus").click(changeValue("-", "#break"));
-    $("#breakPlus").click(changeValue("+", "#break"));
-    $("#sessionMinus").click(changeValue("-", "#session"));
-    $("#sessionPlus").click(changeValue("+", "#session"));
-}
-
-function disableControls() {
-    $("#breakMinus").off("click");
-    $("#breakPlus").off("click");
-    $("#sessionMinus").off("click");
-    $("#sessionPlus").off("click");
-}
-
-$(document).ready(function () {
-    $("#clock").html($("#session").val());
-    $("#clock").click(clickClock("#session"));
-    enableControls();
-});
