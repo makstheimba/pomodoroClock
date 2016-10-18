a62e17b807ceb1cba6a50ca1598d89e7e0870a9b
makstheimba <makstheimba@yandex.ru>
2016-10-19 00:01:20 +0300

    Settings and styling

--- assets/pomodoro.js
+++ assets/pomodoro.js
@@ -1,3 +1,4 @@
+
 var clockMaker = function (clockId) {
    var minute = 60000,
        session = 25*minute,
@@ -5,9 +6,21 @@ var clockMaker = function (clockId) {
        timer = session,
        interval = 1000,
        running = "session",
-       counter = false;
+       counter = false,
+       audioAlarm = new Audio("https://notificationsounds.com/soundfiles/cf67355a3333e6e143439161adc2d82e/file-sounds-874-gets-in-the-way.mp3");
+       
     function convertMs(ms){
         return (("0" + (ms / minute << 0)).slice(-2) + ":" + ("0" + ms % minute / 1000).slice(-2));
+    }
+    
+    function setRunningLabel () {
+        if (running === "rest") {
+            $("#runningLabel").html("BREAK");
+            $("#clockDisplay").removeClass("workSession").addClass(" restSession");
+        } else {
+            $("#runningLabel").html("WORK");
+            $("#clockDisplay").removeClass("restSession").addClass(" workSession");;
+        }        
     }
     
     return {
@@ -23,6 +36,7 @@ var clockMaker = function (clockId) {
                 timer = timer - interval;
                 console.log(timer);
                 if (timer < 0) {
+                    audioAlarm.play();
                     if (running === "session"){
                         timer = rest;
                         running = "rest";
@@ -30,6 +44,7 @@ var clockMaker = function (clockId) {
                         timer = session;
                         running = "session";
                     }
+                    setRunningLabel();
                 }
                 self.setTimerToId();
             }
@@ -47,31 +62,7 @@ var clockMaker = function (clockId) {
             running = "session";
             timer = session;
             this.setTimerToId();
-        },
-        increaseRest: function () {
-            console.log("increase rest");
-            if (rest < minute * 59){
-                rest += minute;
-            }
-            this.resetClock();
-        },
-        decreaseRest: function () {
-            if (rest > minute) {
-                rest -= minute;
-            }            
-            this.resetClock();
-        },
-        increaseSession: function () {
-            if (session < minute * 59) {
-                session += minute;                
-            }
-            this.resetClock();
-        },
-        decreaseSession: function () {
-            if (session > minute) {
-                session -= minute;
-            }
-            this.resetClock();
+            setRunningLabel();
         },
         getSessionLength: function () {
             return convertMs(session);
@@ -79,30 +70,63 @@ var clockMaker = function (clockId) {
         getRestLength: function () {
             return convertMs(rest);
         },
+        setSessionLength: function (len) {
+            session = len * minute;
+            this.resetClock();
+        },
+        setRestLength: function (len) {
+            rest = len * minute;
+            this.resetClock();
+        },
         setTimerToId: function () {
             $(clockId).html(convertMs(timer));
         }
     }
 };
 
-window.onload = function () {
+window.onload = function () {    
+    var clock = clockMaker("#clock"),
+        audioStart = new Audio("https://notificationsounds.com/soundfiles/087408522c31eeb1f982bc0eaf81d35f/file-sounds-949-you-wouldnt-believe.mp3");
     
-    clock = clockMaker("#clock");
-    $("#sessionPlus").on("click", function () {
-        clock.increaseSession();
-        $("#session").val(clock.getSessionLength());
+    $(".clockWrap").on("click", function () {
+        clock.start();
+        audioStart.play();
     });
-    $("#sessionMinus").on("click", function () {
-        clock.decreaseSession();
-        $("#session").val(clock.getSessionLength());
+    $("#btnSettingsShow").on("click", function () {
+        $(".settingsScreen").show();
+        $(this).hide();
     });
-    $("#restPlus").on("click", function () {
-        clock.increaseRest();
-        $("#rest").val(clock.getRestLength());
+    $("#btnSettingsHide").on("click", function () {
+        $(".settingsScreen").hide();
+        $("#btnSettingsShow").show();
     });
-    $("#restMinus").on("click", function () {
-        clock.decreaseRest();
-        $("#rest").val(clock.getRestLength());
-    });
-    $(".clockWrap").on("click", function () {clock.start();});
+    
+    (function () {
+        var baseSlider = {
+            animate: "slow",
+            max: 59,
+            min: 1,
+            range: "max"
+        },
+            sessionSliderOptions = Object.assign({}, baseSlider),
+            restSliderOptions = Object.assign({}, baseSlider);
+        
+        $.extend(sessionSliderOptions, {
+            value: 25,
+            slide: function (event, ui) {
+                $("#sessionLength").html(ui.value + (ui.value === 1 ? " minute" : " minutes"));
+                clock.setSessionLength(ui.value);
+            }
+        });
+        $.extend(restSliderOptions, {
+            value: 5,
+            slide: function (event, ui) {
+                $("#restLength").html(ui.value + (ui.value === 1 ? " minute" : " minutes"));
+                clock.setRestLength(ui.value);
+            }
+        });
+        
+        $("#sessionSlider").slider(sessionSliderOptions);
+        $("#restSlider").slider(restSliderOptions);
+    }());
 }
